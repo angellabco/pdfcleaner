2fee206cae3efbd1f4897cbabde81f18bd30c8c9
angellabco <109616695+angellabco@users.noreply.github.com>
2023-05-29 13:10:21 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import streamlit as st
 from PIL import Image
-from transformers import TrOCRForCausalLM, TrOCREncoderModel
-from transformers import VisionEncoderDecoderModel
+from transformers import VisionEncoderDecoderModel, TrOCRProcessor
 
 @st.cache(allow_output_mutation=True)
 def load_model():
     # Load the TrOCR model
-    encoder = TrOCREncoderModel.from_pretrained("microsoft/trocr-base")
-    decoder = TrOCRForCausalLM.from_pretrained("microsoft/trocr-base")
-    model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
-    return model
+    model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base")
+    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base")
+    return model, processor
 
 def app():
-    model = load_model()
+    model, processor = load_model()
 
     st.title('TrOCR Application')
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "bmp"])
@@ -24,15 +22,17 @@ def app():
 
         # Convert image to the format required by TrOCR
         # Here, a simple conversion is done. More preprocessing might be required depending on the image
-        image = image.convert('RGB')
+        image = processor(document=[image], return_tensors="pt", padding=True)
 
         # Perform OCR with the TrOCR model
-        inputs = model.prepare_inputs_for_generation(image)
-        predictions = model.generate(**inputs)
+        predictions = model.generate(input_ids=image.input_ids, attention_mask=image.attention_mask, decoder_input_ids=image.decoder_input_ids, decoder_attention_mask=image.decoder_attention_mask)
+
+        # Decode the generated tensor into human-readable text
+        predicted_text = processor.batch_decode(predictions, skip_special_tokens=True)
 
         # Display the OCR results
         st.write('Extracted Text:')
-        st.write(predictions)
+        st.write(predicted_text[0])
 
 if __name__ == '__main__':
     app()
