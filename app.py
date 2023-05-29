e16530db3e1f9a69eacbd4024ae6b8d73d39798d
angellabco <109616695+angellabco@users.noreply.github.com>
2023-05-29 12:50:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,38 @@
 import streamlit as st
 from PIL import Image
-import pytesseract
-from pdf2image import convert_from_path
-import cv2
-import os
+from transformers import TrOCRForCausalLM, TrOCREncoderModel
+from transformers import VisionEncoderDecoderModel
 
-st.title('PDF OCR App')
+@st.cache(allow_output_mutation=True)
+def load_model():
+    # Load the TrOCR model
+    encoder = TrOCREncoderModel.from_pretrained("microsoft/trocr-base")
+    decoder = TrOCRForCausalLM.from_pretrained("microsoft/trocr-base")
+    model = VisionEncoderDecoderModel(encoder=encoder, decoder=decoder)
+    return model
 
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
-if uploaded_file is not None:
-    with open("temp.pdf", "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    
-    try:
-        images = convert_from_path('temp.pdf')
-        for i in range(len(images)):
-            # Save pages as images in the pdf
-            images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+def app():
+    model = load_model()
 
-        st.write('Converted PDF to Images')
+    st.title('TrOCR Application')
+    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "bmp"])
 
-        text = ''
-        for i in range(len(images)):
-            # Open image using OpenCV
-            img = cv2.imread('page'+ str(i) +'.jpg')
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.write('')
 
-            # Preprocess the image
-            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            _, img_bin = cv2.threshold(gray_img, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-            img_bin = 255 - img_bin
+        # Convert image to the format required by TrOCR
+        # Here, a simple conversion is done. More preprocessing might be required depending on the image
+        image = image.convert('RGB')
 
-            # Save the preprocessed image
-            cv2.imwrite('preprocessed_page'+ str(i) +'.jpg', img_bin)
+        # Perform OCR with the TrOCR model
+        inputs = model.prepare_inputs_for_generation(image)
+        predictions = model.generate(**inputs)
 
-            # Perform OCR on the preprocessed image
-            text += pytesseract.image_to_string(Image.open('preprocessed_page'+ str(i) +'.jpg'))
+        # Display the OCR results
+        st.write('Extracted Text:')
+        st.write(predictions)
 
-        st.write('Performed OCR on images')
-        st.text_area("Text", text)
-
-        # Clean up temporary files
-        os.remove("temp.pdf")
-        for i in range(len(images)):
-            os.remove('page'+ str(i) +'.jpg')
-            os.remove('preprocessed_page'+ str(i) +'.jpg')
-
-    except Exception as e:
-        st.error(f"Error: {e}")
+if __name__ == '__main__':
+    app()
