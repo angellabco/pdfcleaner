d38ddb381c86c13fadf7125d91770fe8aa05ed9e
angellabco <109616695+angellabco@users.noreply.github.com>
2023-05-29 13:19:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,30 @@
 import streamlit as st
 from PIL import Image
-from transformers import VisionEncoderDecoderModel, TrOCRProcessor
+from transformers import VisionEncoderDecoderModel, BertTokenizerFast
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    # Load the TrOCR model
     model = VisionEncoderDecoderModel.from_pretrained("microsoft/trocr-base")
-    processor = TrOCRProcessor.from_pretrained("microsoft/trocr-base")
-    return model, processor
+    tokenizer = BertTokenizerFast.from_pretrained("microsoft/trocr-base")
+    return model, tokenizer
 
 def app():
-    model, processor = load_model()
+    model, tokenizer = load_model()
 
-    st.title('TrOCR Application')
-    uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png", "bmp"])
+    st.title("TrOCR Demo")
+    uploaded_file = st.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
-        st.write('')
+        st.image(image, caption='Uploaded Image.', use_column_width=True)
+        st.write("")
 
-        # Convert image to the format required by TrOCR
-        # Here, a simple conversion is done. More preprocessing might be required depending on the image
-        image = processor(document=[image], return_tensors="pt", padding=True)
+        inputs = tokenizer(images=image, return_tensors="pt", padding="max_length", truncation=True, max_length=512)
+        outputs = model.generate(**inputs)
+        prediction = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        
+        st.write("OCR Output:")
+        st.write(prediction)
 
-        # Perform OCR with the TrOCR model
-        predictions = model.generate(input_ids=image.input_ids, attention_mask=image.attention_mask, decoder_input_ids=image.decoder_input_ids, decoder_attention_mask=image.decoder_attention_mask)
-
-        # Decode the generated tensor into human-readable text
-        predicted_text = processor.batch_decode(predictions, skip_special_tokens=True)
-
-        # Display the OCR results
-        st.write('Extracted Text:')
-        st.write(predicted_text[0])
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app()
